4eb6e95eee7c866a34e8a3e536430cbbf124cfd4
minhty06 <106997563+minhty06@users.noreply.github.com>
2023-04-10 21:28:33 -0700

    First Commit Blank Project

---
+++ Hobit/Hobit/HobitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobitApp.swift
+//  Hobit
+//
+//  Created by Minh Ton on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HobitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
