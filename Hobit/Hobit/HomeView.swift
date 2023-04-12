a5d17ffdc7e59361966936b74a00c348c7b216c0
minhty06 <106997563+minhty06@users.noreply.github.com>
2023-04-12 12:41:51 -0700

    Added Home View and Nav Bar

---
+++ Hobit/Hobit/HomeView.swift
@@ -0,0 +1,127 @@
+//
+//  HomeView.swift
+//  Hobit
+//
+//  Created by Minh Ton on 4/12/23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Hobit")
+                    .font(.largeTitle.weight(.bold))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .clipped()
+                    .padding(.bottom, 8)
+                Image("Rock")
+                    .renderingMode(.original)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 60, height: 60)
+                    .clipped()
+                    .mask { RoundedRectangle(cornerRadius: 50, style: .continuous) }
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+            VStack(spacing: 40) {
+                VStack {
+                    Text("Routines".uppercased())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .clipped()
+                        .font(.title2.weight(.medium))
+                        .foregroundColor(.primary)
+                    VStack(spacing: 7) {
+                        HStack(spacing: 10) {
+                            Image("Rock2")
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 70, height: 70)
+                                .clipped()
+                                .mask { RoundedRectangle(cornerRadius: 8, style: .continuous) }
+                            VStack(alignment: .leading) {
+                                Text("Routine Placeholder")
+                                    .font(.system(size: 16, weight: .medium, design: .default))
+                                Text("March 9th")
+                                    .font(.footnote)
+                                    .foregroundColor(.secondary)
+                            }
+                            .font(.subheadline)
+                            Spacer()
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.displayP3, red: 234/255, green: 76/255, blue: 97/255))
+                                .font(.title3)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal)
+            }
+            .padding(.vertical)
+            VStack(spacing: 40) {
+                VStack {
+                    Text("Habits".uppercased())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .clipped()
+                        .font(.title2.weight(.medium))
+                        .foregroundColor(.primary)
+                    VStack(spacing: 7) {
+                        //Element here
+                        HabitDashboard()
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal)
+            }
+            .padding(.vertical)
+            VStack(spacing: 40) {
+                VStack {
+                    Text("Goals".uppercased())
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .clipped()
+                        .font(.title2.weight(.medium))
+                        .foregroundColor(.primary)
+                    VStack(spacing: 7) {
+                        HStack(spacing: 10) {
+                            Image("Rock2")
+                                .renderingMode(.original)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 70, height: 70)
+                                .clipped()
+                                .mask { RoundedRectangle(cornerRadius: 8, style: .continuous) }
+                            VStack(alignment: .leading) {
+                                Text("Goals Placeholder")
+                                    .font(.system(size: 16, weight: .medium, design: .default))
+                                Text("March 9th")
+                                    .font(.footnote)
+                                    .foregroundColor(.secondary)
+                            }
+                            .font(.subheadline)
+                            Spacer()
+                            Image(systemName: "ellipsis")
+                                .foregroundColor(Color(.displayP3, red: 234/255, green: 76/255, blue: 97/255))
+                                .font(.title3)
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal)
+            }
+            .padding(.vertical)
+        }
+        .frame(maxWidth: .infinity)
+        .clipped()
+        .padding(.top, 98)
+        .padding(.bottom, 150)
+        }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
