a042d2635a409336ce628847cea958c76aca8d21
rihango1015 <hgao7461@usc.edu>
2023-04-17 16:51:07 -0700

    Update ContentView.swift

--- Hobit/Hobit/ContentView.swift
+++ Hobit/Hobit/ContentView.swift
@@ -8,16 +8,102 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var selectionIndex = 0
+    @State var presented = false
+    
+    let icons = [
+        "house",
+        "clock.badge.checkmark",
+        "plus",
+        "square.and.pencil",
+        "target"
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            // Content
+            ZStack {
+                
+                // Add a blank page for adding new content when "+" is clicked
+                Spacer().fullScreenCover(isPresented: $presented, content: {
+                    VStack {
+                        Button(action: {
+                        presented.toggle()
+                        }, label: {
+                            Text("Close")
+                        })
+                    }
+                })
+                
+                switch selectionIndex{
+                case 0:
+                    NavigationView {
+                        HomeView()
+                    }
+                case 1:
+                    NavigationView {
+                        VStack {
+                            Text("Page 2: shows the full list of routines")
+                        }
+                        .navigationTitle("Routines")
+                    }
+                case 2:
+                    NavigationView {
+                        VStack {
+                            Text("3rd Screen")
+                        }
+                        .navigationTitle("Add")
+                    }
+                case 3:
+                    NavigationView {
+                        VStack {
+                            Text("Page 3: shows the full list of habits")
+                        }
+                        .navigationTitle("Habits")
+                    }
+                default:
+                    NavigationView {
+                        VStack {
+                            Text("Page 4: shows the full list of goals")
+                        }
+                        .navigationTitle("Goals")
+                    }
+                }
+            }
+            
+            HStack {
+                ForEach(0..<5, id: \.self) { number in
+                    Spacer()
+                    Button (action: {
+                        if number == 2 {
+                            presented.toggle()
+                        }
+                        self.selectionIndex = number
+                    }, label: {
+                        if number == 2 {
+                            Image(systemName: icons[number])
+                                .background(.green)
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(50)
+                                .foregroundColor(.white)
+                                .font(.system(size:50, weight: .medium, design: .default)
+                                )
+                        }
+                        
+                        else {
+                            Image(systemName: icons[number])
+                                .foregroundColor(selectionIndex == number ? .green : .gray)
+                                .font(.system(size:25, weight: .medium, design: .rounded))
+                        }
+                    })
+                    Spacer()
+                }
+            }
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
