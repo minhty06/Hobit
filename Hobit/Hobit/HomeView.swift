75670d6c22c7f0a713cd9a8fb64279653765a89c
edward <edwardf1@uci.edu>
2023-04-17 20:07:03 -0700

    Added ScrollView on homeview

--- Hobit/Hobit/HomeView.swift
+++ Hobit/Hobit/HomeView.swift
@@ -9,97 +9,101 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Text("Hobit")
-                    .font(.largeTitle.weight(.bold))
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .clipped()
-                    .padding(.bottom, 8)
-                Image("Rock")
-                    .renderingMode(.original)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 60, height: 60)
-                    .clipped()
-                    .mask { RoundedRectangle(cornerRadius: 50, style: .continuous) }
-            }
-            .padding(.horizontal)
-            .padding(.bottom)
-            VStack(spacing: 40) {
-                VStack {
-                    Text("Routines".uppercased())
+        ScrollView{
+            VStack {
+                HStack {
+                    Text("Hobit")
+                        .font(.largeTitle.weight(.bold))
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .clipped()
-                        .font(.title2.weight(.medium))
-                        .foregroundColor(.primary)
-                    VStack(spacing: 7) {
-                        //Place routine dashboard here
-                        
-                        RoutineDashboard()
-                    }
-                    Spacer()
+                        .padding(.bottom, 8)
+                    Image("Rock")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 60, height: 60)
+                        .clipped()
+                        .mask { RoundedRectangle(cornerRadius: 50, style: .continuous) }
                 }
                 .padding(.horizontal)
-            }
-            .padding(.vertical)
-            VStack(spacing: 40) {
-                VStack {
-                    Text("Habits".uppercased())
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .clipped()
-                        .font(.title2.weight(.medium))
-                        .foregroundColor(.primary)
-                    VStack(spacing: 7) {
-                        //Element here
-                        HabitDashboard()
-                    }
-                    Spacer()
-                }
-                .padding(.horizontal)
-            }
-            .padding(.vertical)
-            VStack(spacing: 40) {
-                VStack {
-                    Text("Goals".uppercased())
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .clipped()
-                        .font(.title2.weight(.medium))
-                        .foregroundColor(.primary)
-                    VStack(spacing: 7) {
-                        HStack(spacing: 10) {
-                            Image("Rock2")
-                                .renderingMode(.original)
-                                .resizable()
-                                .aspectRatio(contentMode: .fill)
-                                .frame(width: 70, height: 70)
-                                .clipped()
-                                .mask { RoundedRectangle(cornerRadius: 8, style: .continuous) }
-                            VStack(alignment: .leading) {
-                                Text("Goals Placeholder")
-                                    .font(.system(size: 16, weight: .medium, design: .default))
-                                Text("March 9th")
-                                    .font(.footnote)
-                                    .foregroundColor(.secondary)
-                            }
-                            .font(.subheadline)
-                            Spacer()
-                            Image(systemName: "ellipsis")
-                                .foregroundColor(Color(.displayP3, red: 234/255, green: 76/255, blue: 97/255))
-                                .font(.title3)
+                .padding(.bottom)
+                VStack(spacing: 40) {
+                    VStack {
+                        Text("Routines".uppercased())
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .clipped()
+                            .font(.title2.weight(.medium))
+                            .foregroundColor(.primary)
+                        VStack(spacing: 7) {
+                            //Place routine dashboard here
+                            
+                            RoutineDashboard()
                         }
+                        Spacer()
                     }
-                    Spacer()
+                    .padding(.horizontal)
                 }
-                .padding(.horizontal)
+                .padding(.vertical)
+                VStack(spacing: 40) {
+                    VStack {
+                        Text("Habits".uppercased())
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .clipped()
+                            .font(.title2.weight(.medium))
+                            .foregroundColor(.primary)
+                        VStack(spacing: 7) {
+                            //Element here
+                            HabitDashboard()
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.vertical)
+                VStack(spacing: 40) {
+                    VStack {
+                        Text("Goals".uppercased())
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .clipped()
+                            .font(.title2.weight(.medium))
+                            .foregroundColor(.primary)
+                        VStack(spacing: 7) {
+                            HStack(spacing: 10) {
+                                Image("Rock2")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: 70, height: 70)
+                                    .clipped()
+                                    .mask { RoundedRectangle(cornerRadius: 8, style: .continuous) }
+                                VStack(alignment: .leading) {
+                                    Text("Goals Placeholder")
+                                        .font(.system(size: 16, weight: .medium, design: .default))
+                                    Text("March 9th")
+                                        .font(.footnote)
+                                        .foregroundColor(.secondary)
+                                }
+                                .font(.subheadline)
+                                Spacer()
+                                Image(systemName: "ellipsis")
+                                    .foregroundColor(Color(.displayP3, red: 234/255, green: 76/255, blue: 97/255))
+                                    .font(.title3)
+                            }
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.vertical)
             }
-            .padding(.vertical)
+            .frame(maxWidth: .infinity)
+            .clipped()
+            .padding(.top, 10)
+            .padding(.bottom, 150)
+            }
+            
         }
-        .frame(maxWidth: .infinity)
-        .clipped()
-        .padding(.top, 98)
-        .padding(.bottom, 150)
-        }
+       
 }
 
 struct HomeView_Previews: PreviewProvider {
