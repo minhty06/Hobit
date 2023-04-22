66991f901570fff567953fc2512f1c7009b0f948
rihango1015 <hgao7461@usc.edu>
2023-04-22 12:40:59 -0700

    Create RoutineView.swift

---
+++ Hobit/Hobit/View/RoutineView.swift
@@ -0,0 +1,50 @@
+//
+//  RoutineView.swift
+//  Hobit
+//
+//  Created by Han Gao on 4/22/23.
+//
+
+import SwiftUI
+
+struct RoutineView: View {
+    @EnvironmentObject var viewModel: RoutineViewModel
+    var body: some View {
+        ScrollView{
+            VStack {
+                HStack {
+                    Text("Routines")
+                        .font(.largeTitle.weight(.bold))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .clipped()
+                        .padding(.bottom, 8)
+                    Image("Rock")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 60, height: 60)
+                        .clipped()
+                        .mask { RoundedRectangle(cornerRadius: 50, style: .continuous) }
+                }
+                .padding(.horizontal)
+                .padding(.bottom)
+                VStack(spacing: 40) {
+                    VStack {
+                        VStack(spacing: 7) {
+                            //Place routine dashboard here
+                            RoutineDashboard(routine: Routine())
+                        }
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                }
+            }
+        }
+    }
+}
+
+struct RoutineView_Previews: PreviewProvider {
+    static var previews: some View {
+        RoutineView()
+    }
+}
